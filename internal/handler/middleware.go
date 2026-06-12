fe8a6a9b261cd0422824e166cbe7e9cfd221b7c5
shriyamverma <shriyamverma@gmail.com>
2026-06-12 14:56:18 +0530

    add: logging on HTTP Request

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -1,6 +1,10 @@
 package handler
 
 import (
+	"bytes"
+	"github.com/go-chi/chi/v5/middleware"
+	"io"
+	"log/slog"
 	"net/http"
 	"os"
 )
@@ -21,11 +25,38 @@ func CORSMiddleware() func(http.Handler) http.Handler {
 
 			// Handle browser preflight checks instantly
 			if r.Method == "OPTIONS" {
+				w.Header().Set("Access-Control-Max-Age", "86400") // 24 hours
 				w.WriteHeader(http.StatusOK)
+				return
+			}
+
+			err := logAndRestoreBody(r)
+			if err != nil {
+				respondWithError(w, r, http.StatusBadRequest, err.Error())
 				return
 			}
 
 			next.ServeHTTP(w, r)
 		})
 	}
+}
+
+func logAndRestoreBody(r *http.Request) error {
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+	r.Body.Close() // Close the original reader
+
+	logFields := []interface{}{
+		"request_id", middleware.GetReqID(r.Context()),
+		"method", r.Method,
+		"path", r.URL.Path,
+		"body", string(bodyBytes),
+	}
+
+	slog.Debug("HTTP Request", logFields...)
+
+	r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+	return nil
 }
